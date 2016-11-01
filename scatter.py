ff078d0a54407e894f877fcaf2a6fcc78a7baf10
fjcamillo <fjcamillo.dev@gmail.com>
2016-11-01 10:45:31 +0800

    moving the files sto ubuntu

--- scatter.py
+++ scatter.py
@@ -2,16 +2,18 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.scatter import Scatter
+from kivy.uix.textinput import TextInput
+from kivy.uix.boxlayout import BoxLayout
+
+
+class ScatterTextWidget(BoxLayout):
+    pass
+
 
 class TutorialApp(App):
-    def build(self):
-        f = FloatLayout()
-        s = Scatter()
-        l = Label(text="Hello!", font_size=150)
 
-        f.add_widget(s)
-        s.add_widget(l)
-        return f
+    def build(self):
+        return ScatterTextWidget()
 
 if __name__ == "__main__":
     TutorialApp().run()
